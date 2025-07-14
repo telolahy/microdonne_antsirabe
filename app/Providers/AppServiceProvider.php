f0508658fa540db45e2c06d5d3cc59887d73689f
telolahy <telodan9@gmail.com>
2025-07-14 08:53:08 +0300

    pagination bootstrap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use App\Download;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::defaultView('pagination::bootstrap-4');
+        Paginator::defaultSimpleView('pagination::simple-bootstrap-4');
         View::composer('*', function ($view) {
             $nouvellesDemandes = Download::where('status', 'en_attente')->count();
             $view->with('nouvellesDemandes', $nouvellesDemandes);
