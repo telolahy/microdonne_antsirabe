c631df83dce33c55cc59611d6a5aaab59485eb3d
RoronoaZ17 <brigade.micky@gmail.com>
2025-07-18 09:01:42 +0300

    title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Microdonne INSTAT</title>
+    <title>Microdonnées INSTAT Madagascar</title>
     <link rel="shortcut icon" href="/logo/logo instat.ico" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
